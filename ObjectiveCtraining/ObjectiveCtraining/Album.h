0b5d0065dfb267c1dcb59182d79454444dc2f16a
MoonJS <blastingzone@naver.com>
2016-03-14 22:59:32 +0900

    add album class

---
+++ ObjectiveCtraining/ObjectiveCtraining/Album.h
@@ -0,0 +1,13 @@
+//
+//  Album.h
+//  ObjectiveCtraining
+//
+//  Created by Moon on 2016. 3. 14..
+//  Copyright © 2016년 Moon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Album : NSObject
+
+@end
---
+++ ObjectiveCtraining/ObjectiveCtraining/Album.m
@@ -0,0 +1,13 @@
+//
+//  Album.m
+//  ObjectiveCtraining
+//
+//  Created by Moon on 2016. 3. 14..
+//  Copyright © 2016년 Moon. All rights reserved.
+//
+
+#import "Album.h"
+
+@implementation Album
+
+@end

f5f358d5b77f63f414e3c1aeb2a3a2256cad846c
MoonJS <blastingzone@naver.com>
2016-03-16 23:56:16 +0900

    add new album class controller files

---
+++ ObjectiveCtraining/ObjectiveCtraining/AlbumDataController.h
@@ -0,0 +1,13 @@
+//
+//  AlbumDataController.h
+//  ObjectiveCtraining
+//
+//  Created by Moon on 2016. 3. 16..
+//  Copyright © 2016년 Moon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AlbumDataController : NSObject
+
+@end
---
+++ ObjectiveCtraining/ObjectiveCtraining/AlbumDataController.m
@@ -0,0 +1,15 @@
+//
+//  AlbumDataController.m
+//  ObjectiveCtraining
+//
+//  Created by Moon on 2016. 3. 16..
+//  Copyright © 2016년 Moon. All rights reserved.
+//
+
+@class Album; // forward declare
+
+#import "AlbumDataController.h"
+
+@implementation AlbumDataController
+
+@end

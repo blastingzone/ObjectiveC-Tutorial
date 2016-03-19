4f5c6d81e8adae4818eb11a1f5655024b4da1dec
MoonJS <blastingzone@naver.com>
2016-03-19 15:45:27 +0900

    add custom table view

---
+++ ObjectiveCTraining/ObjectiveCTraining/CustomTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewController.h
+//  ObjectiveCTraining
+//
+//  Created by MoonJS on 2016. 3. 19..
+//  Copyright © 2016년 Moon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewController : UITableViewController
+
+@end
---
+++ ObjectiveCTraining/ObjectiveCTraining/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  ObjectiveCTraining
+//
+//  Created by MoonJS on 2016. 3. 19..
+//  Copyright © 2016년 Moon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end

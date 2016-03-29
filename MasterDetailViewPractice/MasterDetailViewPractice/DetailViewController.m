19cc5163dc1eaa4a8a0eb236c37c0121b2b72a1b
MoonJS <blastingzone@naver.com>
2016-03-30 01:41:35 +0900

    replace detail view to table-detail-view (not done)

--- MasterDetailViewPractice/MasterDetailViewPractice/DetailViewController.m
+++ MasterDetailViewPractice/MasterDetailViewPractice/DetailViewController.m
@@ -27,9 +27,6 @@
 
 - (void)configureView {
     // Update the user interface for the detail item.
-    if (self.detailItem) {
-        self.detailDescriptionLabel.text = [self.detailItem description];
-    }
 }
 
 - (void)viewDidLoad {

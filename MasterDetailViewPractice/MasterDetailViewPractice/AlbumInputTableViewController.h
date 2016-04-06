ccf11e144845a981e3674f3deaa8ead7294e86e7
MoonJS <blastingzone@naver.com>
2016-04-07 01:54:11 +0900

    add custom album input cell controller (blank)

---
+++ MasterDetailViewPractice/MasterDetailViewPractice/AlbumInputTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlbumInputTableViewController.h
+//  MasterDetailViewPractice
+//
+//  Created by Moon on 2016. 4. 6..
+//  Copyright © 2016년 Moon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlbumInputTableViewController : UITableViewController
+
+@end

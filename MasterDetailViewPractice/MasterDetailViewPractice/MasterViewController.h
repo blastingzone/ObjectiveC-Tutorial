48975f1322264ed13784b53c48d6313ef09234ca
MoonJS <blastingzone@naver.com>
2016-04-11 00:38:18 +0900

    add managed object context

--- MasterDetailViewPractice/MasterDetailViewPractice/MasterViewController.h
+++ MasterDetailViewPractice/MasterDetailViewPractice/MasterViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
 
 @class DetailViewController;
 
@@ -14,6 +15,8 @@
 
 @property (strong, nonatomic) DetailViewController *detailViewController;
 
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
 
 @end
 

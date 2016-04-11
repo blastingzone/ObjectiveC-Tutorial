a51268adfbb189f9e5317f6bade616e24fbc604e
MoonJS <blastingzone@naver.com>
2016-04-12 07:53:12 +0900

    objective c terminal helloworld

---
+++ cmd_helloworld/cmd_helloworld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  cmd_helloworld
+//
+//  Created by Moon on 2016. 4. 12..
+//  Copyright © 2016년 Moon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

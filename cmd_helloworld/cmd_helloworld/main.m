53c8c8ebc30755a6d482862abce7c4ec88a3eaf6
MoonJS <blastingzone@naver.com>
2016-05-07 23:55:13 +0900

    NSNumber example (from book)

--- cmd_helloworld/cmd_helloworld/main.m
+++ cmd_helloworld/cmd_helloworld/main.m
@@ -43,15 +43,45 @@ typedef Fraction* Frac_ptr;
 typedef unsigned int Counter;
 
 
-void (^helloWorld) (NSString*, NSString*) =
-^(NSString *name, NSString* location){
-    NSLog(@"Hello World, %@ from %@", name, location);
-};
-
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSNumber *myNumber, *floatNumber, *intNumber;
+        NSInteger myInt;
         
-        helloWorld(@"Frodo", @"Shire");
+        // integer value
+        intNumber = [NSNumber numberWithInteger: 100];
+        myInt = [intNumber integerValue];
+        NSLog (@"%li", (long) myInt);
+        
+        // long value
+        myNumber = [NSNumber numberWithLong: 0xabcdef];
+        NSLog (@"%lx", [myNumber longValue]);
+        
+        // char value
+        myNumber = [NSNumber numberWithChar: 'X'];
+        NSLog (@"%c", [myNumber charValue]);
+        NSLog (@"%d", [myNumber charValue]);
+        
+        // float value
+        floatNumber = [NSNumber numberWithFloat: 100.00];
+        NSLog (@"%g", [floatNumber floatValue]);
+        
+        // double
+        myNumber = [NSNumber numberWithDouble: 12345e+15];
+        NSLog (@"%lg", [myNumber doubleValue]);
+        
+        // Wrong access here
+        NSLog (@"%li", (long) [myNumber integerValue]);
+        
+        // Test two Numbers for equality
+        if ([intNumber isEqualToNumber: floatNumber] == YES)
+            NSLog (@"Numbers are equal");
+        else
+            NSLog (@"Numbers are not equal");
+        
+        // Test if one Number is <, ==, or > second Number
+        if ([intNumber compare: myNumber] == NSOrderedAscending)
+            NSLog (@"First number is less than second");
     }
     return 0;
 }
